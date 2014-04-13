85324190bd0875773ae8f7d379816e5ac9f5af98
Russell White <rarctem@gmail.com>
2014-04-13 13:31:07 -0600

    Basic window setup in Renderer.

--- Prototypes/OrbitSim/src/gui/menu/Renderer.java
+++ Prototypes/OrbitSim/src/gui/menu/Renderer.java
@@ -5,35 +5,73 @@ package gui.menu;
 
 import gui.GUIObject;
 
+import java.awt.Canvas;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
+
 /**
  * @author russell
- *
+ * 
  */
-public class Renderer {
+public class Renderer extends Canvas {
 
 	private Graphics2D g;
 	private ArrayList<GUIObject> guiObjects;
-	
+	private JFrame window;
+
+	public static void main(String args[]) {
+		Renderer rend = new Renderer();
+		while (rend.getWindow().isActive()) {
+			// rend.render();
+			rend.repaint();
+
+			try {
+				Thread.sleep(50);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	/**
 	 * 
 	 */
 	public Renderer() {
-		// TODO Auto-generated constructor stub
+		this.window = new JFrame("Orbit Simulator");
+		this.setSize(800, 600);
+		this.window.add(this);
+		this.window.setFocusable(true);
+		this.window.setResizable(false);
+		this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.window.pack();
+		this.window.setVisible(true);
+
 	}
-	
+
+	public void paint(Graphics screen) {
+		screen.drawRect(20, 20, 300, 200);
+	}
+
 	public void render() {
-		
+
 	}
-	
+
 	public void initialize() {
-		
+
 	}
-	
+
 	public void stop() {
-		
+
+	}
+
+	/**
+	 * @return the window
+	 */
+	public JFrame getWindow() {
+		return window;
 	}
 
 }
