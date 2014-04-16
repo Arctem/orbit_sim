f414b723b8b0d13ca38e08d4823181256ba1c939
Russell White <rarctem@gmail.com>
2014-04-16 12:06:26 -0600

    Add infrastructure for drawing different parts of view.

--- Prototypes/OrbitSim/src/gui/menu/Renderer.java
+++ Prototypes/OrbitSim/src/gui/menu/Renderer.java
@@ -6,6 +6,7 @@ package gui.menu;
 import gui.GUIObject;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
@@ -19,12 +20,13 @@ import javax.swing.JFrame;
  */
 public class Renderer extends Canvas {
 
+	public static final int MENU_SIZE = 300;
 	private ArrayList<GUIObject> guiObjects;
 	private JFrame window;
 
 	public static void main(String args[]) {
 		Renderer rend = new Renderer();
-		while (rend.getWindow().isActive()) {
+		while (rend.getWindow().isEnabled()) {
 			// rend.render();
 			rend.repaint();
 
@@ -55,10 +57,34 @@ public class Renderer extends Canvas {
 				this.getSize().height, BufferedImage.TYPE_3BYTE_BGR);
 		Graphics2D g = buffer.createGraphics();
 
-		g.drawRect(this.getSize().width / 4, this.getSize().height / 4,
-				this.getSize().width / 2, this.getSize().height / 2);
+		BufferedImage menuBuffer = new BufferedImage(MENU_SIZE,
+				this.getSize().height, BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics2D gMenu = menuBuffer.createGraphics();
+
+		BufferedImage mainScreenBuffer = new BufferedImage(
+				this.getSize().width, this.getSize().height,
+				BufferedImage.TYPE_4BYTE_ABGR);
+		Graphics2D gMain = mainScreenBuffer.createGraphics();
+
+		gMain.setColor(Color.green);
+		gMain.fillRect(200, 200, 500, 500);
+
+		
+		gMenu.setColor(new Color(0, 0,
+				(int) Math.abs((System.currentTimeMillis() / 10) % 255), 150));
+		gMenu.fillRect(0, 0, MENU_SIZE, this.getSize().height);
+
+		g.drawImage(mainScreenBuffer, 0, 0, null);
+		g.drawImage(menuBuffer, this.getSize().width - MENU_SIZE, 0, null);
+
+		// g.drawRect(this.getSize().width / 4, this.getSize().height / 4,
+		// this.getSize().width / 2, this.getSize().height / 2);
 
 		screen.drawImage(buffer, 0, 0, null);
+	}
+
+	public void update(Graphics g) {
+		paint(g);
 	}
 
 	public void render() {
