b8a0b58920890b987152f8c896637a857bce4ee7
Russell White <rarctem@gmail.com>
2014-04-24 18:14:09 -0600

    Remove non-OpenGL stuff.

--- Prototypes/OrbitSim/src/gui/menu/Renderer.java
+++ Prototypes/OrbitSim/src/gui/menu/Renderer.java
@@ -38,71 +38,53 @@ public class Renderer extends Canvas {
 
 	public static final int MENU_SIZE = 300;
 	private ArrayList<GUIObject> guiObjects;
-	private JFrame window;
 
 	// ----------- Variables added for Lighting Test -----------//
 	private static FloatBuffer matSpecular, lightPosition, whiteLight,
 			lModelAmbient;
 
-
 	public static void main(String args[]) {
-		if (LWJGL) {
-			Renderer.initGL();
-			Sun sun = new Sun(0, 0, new Point3D(0, 0, 0), Color.YELLOW, 100);
-			Planet planet = new Planet(0, SolarSystem.ASTRONOMICAL_UNIT,
-					Color.GREEN, sun, 10);
+		Renderer.initGL();
+		Sun sun = new Sun(0, 0, new Point3D(0, 0, 0), Color.YELLOW, 100);
+		Planet planet = new Planet(0, SolarSystem.ASTRONOMICAL_UNIT,
+				Color.GREEN, sun, 10);
 
-			GL11.glTranslatef(0, 0, 3f);
+		GL11.glTranslatef(0, 0, 3f);
 
-			while (!Display.isCloseRequested()) {
-				// Render stuff.
-				GL11.glClear(GL11.GL_COLOR_BUFFER_BIT
-						| GL11.GL_DEPTH_BUFFER_BIT);
+		while (!Display.isCloseRequested()) {
+			// Render stuff.
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 
-				// GL11.glTranslatef(0, 0, 0.05f);
-				GL11.glRotatef(0.5f, 1f, 0f, 0f);
+			// GL11.glTranslatef(0, 0, 0.05f);
+			GL11.glRotatef(0.5f, 1f, 0f, 0f);
 
-				/*
-				 * GL11.glBegin(GL11.GL_POINTS); for (Point p : points) { //
-				 * Draw the point at its coordinates GL11.glVertex3f(p.x, p.y,
-				 * p.z); } GL11.glEnd();
-				 */
+			/*
+			 * GL11.glBegin(GL11.GL_POINTS); for (Point p : points) { // Draw
+			 * the point at its coordinates GL11.glVertex3f(p.x, p.y, p.z); }
+			 * GL11.glEnd();
+			 */
 
-				GL11.glBegin(GL11.GL_LINE_LOOP);
-				GL11.glVertex3f(-0.5f, -0.5f, -10f);
-				GL11.glVertex3f(-0.5f, 0.5f, -10f);
-				GL11.glVertex3f(0.5f, 0.5f, -10f);
-				GL11.glVertex3f(0.5f, -0.5f, -10f);
-				GL11.glEnd();
+			GL11.glBegin(GL11.GL_LINE_LOOP);
+			GL11.glVertex3f(-0.5f, -0.5f, -10f);
+			GL11.glVertex3f(-0.5f, 0.5f, -10f);
+			GL11.glVertex3f(0.5f, 0.5f, -10f);
+			GL11.glVertex3f(0.5f, -0.5f, -10f);
+			GL11.glEnd();
 
-				Renderer.renderSphere(-0.5f, 0.0f, -5f, 0.4f);
-				Renderer.renderSphere(0.5f, 0.0f, -5f, 0.4f);
+			Renderer.renderSphere(-0.5f, 0.0f, -5f, 0.4f);
+			Renderer.renderSphere(0.5f, 0.0f, -5f, 0.4f);
 
-				GL11.glBegin(GL11.GL_TRIANGLES);
-				GL11.glVertex3f(-0.5f, -0.5f, -6f);
-				GL11.glVertex3f(-0.5f, 0.5f, -5f);
-				GL11.glVertex3f(0.5f, 0.5f, -4f);
-				GL11.glEnd();
+			GL11.glBegin(GL11.GL_TRIANGLES);
+			GL11.glVertex3f(-0.5f, -0.5f, -6f);
+			GL11.glVertex3f(-0.5f, 0.5f, -5f);
+			GL11.glVertex3f(0.5f, 0.5f, -4f);
+			GL11.glEnd();
 
-				Display.update();
-				Display.sync(60);
-			}
-
-			Display.destroy();
-		} else {
-
-			Renderer rend = new Renderer();
-			while (rend.getWindow().isEnabled()) {
-				// rend.render();
-				rend.repaint();
-
-				try {
-					Thread.sleep(50);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
+			Display.update();
+			Display.sync(60);
 		}
+
+		Display.destroy();
 	}
 
 	private static void initGL() {
@@ -124,8 +106,9 @@ public class Renderer extends Canvas {
 
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
-		GLU.gluPerspective(fov, (float) Display.getWidth()
-				/ (float) Display.getHeight(), zNear, zFar);
+		GLU.gluPerspective(fov,
+				(float) Display.getWidth() / (float) Display.getHeight(),
+				zNear, zFar);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 		GL11.glLoadIdentity();
 
@@ -133,7 +116,6 @@ public class Renderer extends Canvas {
 		// of the points that are farther away.
 		GL11.glEnable(GL11.GL_DEPTH_TEST);
 
-		
 		Renderer.initLightArrays();
 		GL11.glShadeModel(GL11.GL_SMOOTH);
 		GL11.glMaterial(GL11.GL_FRONT, GL11.GL_SPECULAR, matSpecular);
