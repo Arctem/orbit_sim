c820999621f996a8c745c33caa61c774d615e07b
Russell White <rarctem@gmail.com>
2014-04-24 18:18:07 -0600

    Removed the non-LWJGL stuff I missed.

--- Prototypes/OrbitSim/src/gui/menu/Renderer.java
+++ Prototypes/OrbitSim/src/gui/menu/Renderer.java
@@ -89,7 +89,7 @@ public class Renderer extends Canvas {
 
 	private static void initGL() {
 		try {
-			Display.setDisplayMode(new DisplayMode(800, 600));
+			Display.setDisplayMode(new DisplayMode(1600, 1000));
 			Display.setTitle("Orbit Simulator");
 			Display.create();
 
@@ -162,50 +162,6 @@ public class Renderer extends Canvas {
 				* scalar, position.getZ() * scalar, radius);
 	}
 
-	/**
-	 * 
-	 */
-	public Renderer() {
-		this.window = new JFrame("Orbit Simulator");
-		this.setSize(800, 600);
-		this.window.add(this);
-		this.window.setFocusable(true);
-		this.window.setResizable(true);
-		this.window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.window.pack();
-		this.window.setVisible(true);
-	}
-
-	public void paint(Graphics screen) {
-		BufferedImage buffer = new BufferedImage(this.getSize().width,
-				this.getSize().height, BufferedImage.TYPE_3BYTE_BGR);
-		Graphics2D g = buffer.createGraphics();
-
-		BufferedImage menuBuffer = new BufferedImage(MENU_SIZE,
-				this.getSize().height, BufferedImage.TYPE_4BYTE_ABGR);
-		Graphics2D gMenu = menuBuffer.createGraphics();
-
-		BufferedImage mainScreenBuffer = new BufferedImage(
-				this.getSize().width, this.getSize().height,
-				BufferedImage.TYPE_4BYTE_ABGR);
-		Graphics2D gMain = mainScreenBuffer.createGraphics();
-
-		gMain.setColor(Color.green);
-		gMain.fillRect(200, 200, 500, 500);
-
-		gMenu.setColor(new Color(0, 0, (int) Math.abs((System
-				.currentTimeMillis() / 10) % 255), 150));
-		gMenu.fillRect(0, 0, MENU_SIZE, this.getSize().height);
-
-		g.drawImage(mainScreenBuffer, 0, 0, null);
-		g.drawImage(menuBuffer, this.getSize().width - MENU_SIZE, 0, null);
-
-		// g.drawRect(this.getSize().width / 4, this.getSize().height / 4,
-		// this.getSize().width / 2, this.getSize().height / 2);
-
-		screen.drawImage(buffer, 0, 0, null);
-	}
-
 	public void update(Graphics g) {
 		paint(g);
 	}
@@ -220,13 +176,6 @@ public class Renderer extends Canvas {
 
 	public void stop() {
 
-	}
-
-	/**
-	 * @return the window
-	 */
-	public JFrame getWindow() {
-		return window;
 	}
 
 }
